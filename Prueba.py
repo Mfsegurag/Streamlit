88391919bb91426430e84ac9f3241dcb28f321df
Mfsegurag <94091515+Mfsegurag@users.noreply.github.com>
2023-10-12 17:06:48 -0500

    Add files via upload

--- Prueba.py
+++ Prueba.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
+import openpyxl
 
-import pip
-pip.main(["install","openpyxl"])
+#import pip
+#pip.main(["install","openpyxl"])
 
 st.title('Titulo')
 
 df = pd.read_excel('DATA_PRUEBA.xlsx')
 
-#st.write(df)
+st.write(df)
