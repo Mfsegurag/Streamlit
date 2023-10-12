aef326b2d952c3287b31472f0b93fe43d2e38273
Mfsegurag <94091515+Mfsegurag@users.noreply.github.com>
2023-10-12 15:06:40 -0500

    Add files via upload

--- Prueba.py
+++ Prueba.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
-#import openpyxl
+
 import pip
 pip.main(["install", "openpyxl"])
 
 st.title('Titulo')
 
 df = pd.read_excel('Record_cursos.xlsx')
+
+st.write(df)
